2176002740ca087769b8590fd06904900b7cb111
Aishwary Gupta <guptashark@gmail.com>
2019-08-28 01:00:06 -0400

    working array template class

---
+++ ag_array.h
@@ -0,0 +1,23 @@
+#ifndef AG_ARRAY_H
+#define AG_ARRAY_H
+
+namespace ag_std
+{
+
+	template 
+	<typename T, std::size_t N>
+	class array {
+	private:
+		T m_data[N];
+		const std::size_t m_N = N;
+
+
+
+
+	
+		
+
+
+	};
+} // namespace ag_std
+#endif
--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream> 
 
+#include "ag_array.h"
+
+
+
 int main(int argc, char *argv[]) {
 	
 	(void)argc;
 	(void)argv;
+
+	ag_std::array<int, 5> a;
+	(void)a;
 
 	std::cout << "Udemy Data Structures and Algorithms" << std::endl;
 	return 0;
